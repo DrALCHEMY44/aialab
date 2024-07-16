5bdb6123818c87fa89d1fc91f64b1e8b746832cb
DrALCHEMY44 <kevintchinde36@gmail.com>
2024-07-15 22:57:29 -0700

    Your commit message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,17 @@ import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
 import { socialMedia } from '@/data'
 import { profile } from 'console'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
         <div className="w-full absolute left-0 -bottom-72 min-h-96">
-            <img 
+            <Image
             src="/footer-grid.svg" alt="grid" 
             className="w-full h-full opacity-50"
+            width={400}
+            height={400}
             />
         </div>
         <div className=" flex flex-col items-center">
@@ -19,7 +22,7 @@ const Footer = () => {
             </h1>
             <p className="text-white-200 md:mt-10 my-5 text-cemter">Reach out to us today and let's discuss how I can help you archieve your goals</p>
             <a href="mailto:aialab@gmail.com">
-                <MagicButton title={"Let's get in touch"} icon={<FaLocationArrow/>} position={"right"}/>
+                <MagicButton title={"Lets get in touch"} icon={<FaLocationArrow/>} position={"right"}/>
             </a>
         </div>
 
@@ -29,7 +32,7 @@ const Footer = () => {
             <div className="flex items-center md:gap-3 gap-6">
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border-black-300">
-                        <img src={profile.img} alt={profile.id.toString()} width={20} height={20} />
+                        <Image src={profile.img} alt={profile.id.toString()} width={20} height={20} />
                     </div>
                 ))}
             </div>
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -9,6 +9,7 @@ import Lottie from "react-lottie";
 import animationData from '@/data/conffeti.json';
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
  
 export const BentoGrid = ({
   className,
@@ -74,12 +75,12 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && 'flex justify-center'} h-full`}>
           <div className="w-full h-full absolute">
             {img &&(
-                <img src={img} alt={img}  className={cn(imgClassName,'object-cover, object-center')}/>
+                <Image src={img} alt={img}  className={cn(imgClassName,'object-cover, object-center')} width={400} height={400}/>
             )}
           </div>
           <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
             {spareImg &&(
-                <img src={spareImg} alt={spareImg} className={'object-cover, object-center w-full h-full'} />
+                <Image src={spareImg} alt={spareImg} className={'object-cover, object-center w-full h-full'} width={400} height={400} />
             )}
           </div>
           {id === 6 &&(
